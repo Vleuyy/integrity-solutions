55f886b20106f95b172d35d0e5ab63091620115e
zyzk <85763061+Vleuyy@users.noreply.github.com>
2023-10-21 21:04:25 +0100

    Add files via upload

---
+++ lol.lua
@@ -0,0 +1,21 @@
+wait(1.2)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Integrity Solutions!"; -- the title (ofc)
+Text = "Thanks For Purchasing Our Script!, - I.S v1"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+wait(5)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Integrity Solutions!"; -- the title (ofc)
+Text = "FPS Unlocker Loaded!, - I.S v1"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
+wait(5)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Integrity Solutions!"; -- the title (ofc)
+Text = "Silent Aim Loaded!, - I.S v1"; -- what the text says (ofc)
+Icon = "rbxassetid://57254792"; -- the image if u want. 
+Duration = 5; -- how long the notification should in secounds
+})
